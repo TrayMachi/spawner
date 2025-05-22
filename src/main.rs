8b54c5c28c53d6af3d4dec9bdfe1bc69d0d130cc
TrayMachi <tristan.agra@ui.ac.id>
2025-05-23 06:42:01 +0700

    Experiment 1.2: Understanding how it works

--- src/main.rs
+++ src/main.rs
@@ -106,7 +106,7 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Tristan Hehe: done!");
     });
-
+    println!("Tristan Hehe: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
